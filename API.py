fd43d59f00fe54ea8f524f4eb60921046c575cd5
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-11-30 14:34:04 +0100

    change to openid connect

--- API.py
+++ API.py
@@ -1,71 +1,69 @@
-#LinkedIn API
-
 import streamlit as st
-import requests
 from requests_oauthlib import OAuth2Session
-from urllib.parse import urlparse, parse_qs
+from requests.exceptions import HTTPError
 
 # Constants
 CLIENT_ID = '785jejrypgi7ks'
 CLIENT_SECRET = '4ZwcgJ0s0ENgcVuA'
-REDIRECT_URI = 'https://cz5daz6qukdskgv9cj6h4t.streamlit.app/'  # Your Streamlit app's address
+REDIRECT_URI = 'https://sjkxobe5kjars9kxee5wyb.streamlit.app/'  # Your Streamlit app's address
 AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
 TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-SCOPE = ['r_liteprofile']  # Scope for basic profile information
+SCOPE = ['openid', 'profile', 'email']  # Scopes for OpenID Connect
 
 # Function to parse query string
 def get_query_params():
-    query_params = st.experimental_get_query_params()
-    return query_params
+    return st.experimental_get_query_params()
 
 # Start the OAuth process
 def start_oauth():
     linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
     authorization_url, state = linkedin.authorization_url(AUTHORIZATION_BASE_URL)
-
     # Save the state in session for later use
     st.session_state['oauth_state'] = state
-
     # Redirect to LinkedIn for authorization
-    st.markdown(f"[Log in with LinkedIn to prefill your CV]({authorization_url})", unsafe_allow_html=True)
+    st.markdown(f"[Log in with LinkedIn]({authorization_url})", unsafe_allow_html=True)
 
-# Fetch token
-def fetch_token(code):
-    linkedin = OAuth2Session(CLIENT_ID, state=st.session_state['oauth_state'], redirect_uri=REDIRECT_URI)
-    token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
+# Fetch token and user info
+def fetch_token_and_user_info(code):
+    try:
+        linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+        token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
+        # Save the token in session
+        st.session_state['oauth_token'] = token
 
-    # Save the token in session
-    st.session_state['oauth_token'] = token
-
-    # Fetch basic profile information
-    fetch_profile_info(token)
-
-def fetch_profile_info(token):
-    linkedin = OAuth2Session(CLIENT_ID, token=token)
-    profile_response = linkedin.get('https://api.linkedin.com/v2/me')
-    profile_data = profile_response.json()
-    st.session_state['profile_data'] = profile_data
+        # Fetch user info
+        user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
+        st.session_state['user_info'] = user_info
+        
+        # Fetch user email
+        email_info = linkedin.get('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))').json()
+        st.session_state['email_info'] = email_info.get('elements', [])[0].get('handle~', {}).get('emailAddress', '')
+        
+    except HTTPError as e:
+        st.error(f'An HTTP error occurred: {e.response.status_code}')
+    except Exception as e:
+        st.error(f'An error occurred: {e}')
 
 # Main App
 def main():
-    st.title("Retrieve your LinkedIn Profile Information")
+    st.title("LinkedIn OpenID Connect Authentication")
 
-    # Check if the code is returned in the query string
     query_params = get_query_params()
     code = query_params.get("code")
 
     if "oauth_token" not in st.session_state:
-        # If we don't have an oauth token, and the code is not in the query string, start the OAuth process
         if not code:
             start_oauth()
         else:
-            # If we have the code, fetch the token
-            fetch_token(code[0])
-            st.write("Authentication successful!")
+            fetch_token_and_user_info(code[0])
+            st.success("Authentication successful!")
 
-    if "profile_data" in st.session_state:
-        # If we have the profile data, display it
-        st.write("Your profile data:", st.session_state['profile_data'])
+    if "user_info" in st.session_state:
+        st.write("Your LinkedIn profile information:")
+        st.json(st.session_state['user_info'])
+
+        st.write("Your LinkedIn email information:")
+        st.write(st.session_state['email_info'])
 
 if __name__ == "__main__":
     main()
