768382d311070095ad99b65e26b2ac3ec40f0a1b
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-11-30 16:18:15 +0100

    j

--- API.py
+++ API.py
@@ -27,6 +27,7 @@ def start_oauth():
 def fetch_token_and_user_info(code):
     try:
         linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+        # Add the client_secret parameter to the fetch_token() method call
         token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
         # Save the token in session
         st.session_state['oauth_token'] = token
@@ -34,11 +35,11 @@ def fetch_token_and_user_info(code):
         # Fetch user info
         user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
         st.session_state['user_info'] = user_info
-        
+
         # Fetch user email
         email_info = linkedin.get('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))').json()
         st.session_state['email_info'] = email_info.get('elements', [])[0].get('handle~', {}).get('emailAddress', '')
-        
+
     except HTTPError as e:
         st.error(f'An HTTP error occurred: {e.response.status_code}')
     except Exception as e:
