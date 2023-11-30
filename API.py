68b80ffd55e84ea427cc795ddf9c3408f8e56bf6
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-11-30 17:28:44 +0100

    s

--- API.py
+++ API.py
@@ -29,30 +29,12 @@ def start_oauth():
 def fetch_token_and_user_info(code):
     try:
         linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
-        # Exchange code for token
-        token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
-
+        # Add the client_secret parameter to the fetch_token() method call
+        token = linkedin.fetch_token(TOKEN_URL, client_secret='4ZwcgJ0s0ENgcVuA', code=code)
         # Save the token in session
         st.session_state['oauth_token'] = token
 
-        # Decode and verify the id_token if it's part of the token response
-        if 'id_token' in token:
-            # Get the JWKS client
-            jwks_client = PyJWKClient('https://www.linkedin.com/oauth/openid/jwks')
-            
-            # Get the signing key from the JWKS
-            signing_key = jwks_client.get_signing_key_from_jwt(token['id_token'])
-            
-            # Decode the id_token
-            id_token_data = jwt.decode(
-                token['id_token'], 
-                signing_key.key, 
-                algorithms=["RS256"], 
-                audience=CLIENT_ID
-            )
-            st.session_state['id_token_data'] = id_token_data
-
-        # Fetch user info using the access token
+        # Fetch user info
         user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
         st.session_state['user_info'] = user_info
 
@@ -62,8 +44,6 @@ def fetch_token_and_user_info(code):
 
     except HTTPError as e:
         st.error(f'An HTTP error occurred: {e.response.status_code}')
-    except jwt.JWTError as e:
-        st.error(f'An error occurred while verifying the ID token: {e}')
     except Exception as e:
         st.error(f'An error occurred: {e}')
 
