6f7cc096a9aef03a5ea0535e4a144125551f6e59
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-11-30 14:05:08 +0100

    update redirect uri

--- API.py
+++ API.py
@@ -8,7 +8,7 @@ from urllib.parse import urlparse, parse_qs
 # Constants
 CLIENT_ID = '785jejrypgi7ks'
 CLIENT_SECRET = '4ZwcgJ0s0ENgcVuA'
-REDIRECT_URI = 'https://sjkxobe5kjars9kxee5wyb.streamlit.app/'  # Your Streamlit app's address
+REDIRECT_URI = 'https://cz5daz6qukdskgv9cj6h4t.streamlit.app/'  # Your Streamlit app's address
 AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
 TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
 SCOPE = ['r_liteprofile']  # Scope for basic profile information
