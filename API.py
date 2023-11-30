ff184e27474817407a61f8458ef0a8898f8b71a4
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-11-30 17:20:44 +0100

    g

--- API.py
+++ API.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from requests_oauthlib import OAuth2Session
 from requests.exceptions import HTTPError
+from jwt import PyJWKClient
+import jwt
 
 # Constants
 CLIENT_ID = '785jejrypgi7ks'
@@ -8,7 +10,7 @@ CLIENT_SECRET = '4ZwcgJ0s0ENgcVuA'
 REDIRECT_URI = 'https://cz5daz6qukdskgv9cj6h4t.streamlit.app/'  # Your Streamlit app's address
 AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
 TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-SCOPE = ['openid', 'profile', 'email']  # Scopes for OpenID Connect
+SCOPE = ['r_liteprofile', 'r_emailaddress']  # Scopes for OpenID Connect
 
 # Function to parse query string
 def get_query_params():
@@ -27,12 +29,30 @@ def start_oauth():
 def fetch_token_and_user_info(code):
     try:
         linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
-        # Add the client_secret parameter to the fetch_token() method call
-        token = linkedin.fetch_token(TOKEN_URL, client_secret='4ZwcgJ0s0ENgcVuA', code=code)
+        # Exchange code for token
+        token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
+
         # Save the token in session
         st.session_state['oauth_token'] = token
 
-        # Fetch user info
+        # Decode and verify the id_token if it's part of the token response
+        if 'id_token' in token:
+            # Get the JWKS client
+            jwks_client = PyJWKClient('https://www.linkedin.com/oauth/openid/jwks')
+            
+            # Get the signing key from the JWKS
+            signing_key = jwks_client.get_signing_key_from_jwt(token['id_token'])
+            
+            # Decode the id_token
+            id_token_data = jwt.decode(
+                token['id_token'], 
+                signing_key.key, 
+                algorithms=["RS256"], 
+                audience=CLIENT_ID
+            )
+            st.session_state['id_token_data'] = id_token_data
+
+        # Fetch user info using the access token
         user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
         st.session_state['user_info'] = user_info
 
@@ -42,6 +62,8 @@ def fetch_token_and_user_info(code):
 
     except HTTPError as e:
         st.error(f'An HTTP error occurred: {e.response.status_code}')
+    except jwt.JWTError as e:
+        st.error(f'An error occurred while verifying the ID token: {e}')
     except Exception as e:
         st.error(f'An error occurred: {e}')
 
@@ -65,6 +87,11 @@ def main():
 
         st.write("Your LinkedIn email information:")
         st.write(st.session_state['email_info'])
+
+        # Optionally, display the decoded id_token data
+        if "id_token_data" in st.session_state:
+            st.write("Your LinkedIn ID token data:")
+            st.json(st.session_state['id_token_data'])
 
 if __name__ == "__main__":
     main()
