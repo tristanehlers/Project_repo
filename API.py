9f9b1db0780c340ef4367c16fc9c31fde5fc688c
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-03 19:49:00 +0100

    g

--- API.py
+++ API.py
@@ -5,46 +5,39 @@ from requests.exceptions import HTTPError
 # Constants
 CLIENT_ID = '785jejrypgi7ks'
 CLIENT_SECRET = '4ZwcgJ0s0ENgcVuA'
-REDIRECT_URI = 'https://kup7u2ixdrj2gdn6wmq3er.streamlit.app/'  # Ensure this matches exactly with the LinkedIn application settings
+REDIRECT_URI = 'https://kup7u2ixdrj2gdn6wmq3er.streamlit.app/'
 AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
 TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-SCOPE = 'openid profile email'  # Scopes for OpenID Connect
+SCOPE = 'openid profile email'
 
 # Function to parse query string
 def get_query_params():
     params = st.experimental_get_query_params()
-    return {k: v[0] for k, v in params.items()}
+    return {k: v[0] for k, v in params.items() if v}
 
 # Start the OAuth process
 def start_oauth():
     linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
     authorization_url, state = linkedin.authorization_url(AUTHORIZATION_BASE_URL)
-    # Save the state in session for later use
     st.session_state['oauth_state'] = state
-    # Redirect to LinkedIn for authorization
     st.markdown(f"[Log in with LinkedIn]({authorization_url})", unsafe_allow_html=True)
 
 # Fetch token and user info
 def fetch_token_and_user_info(code):
     try:
         linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
-        # Print the authorization code for debugging
-        st.write("Received authorization code:", code)
-        # Fetch the token using the authorization code
+        st.write("Received authorization code:", code)  # Debug print
         token = linkedin.fetch_token(
             TOKEN_URL,
             client_secret=CLIENT_SECRET,
             code=code,
             headers={'Content-Type': 'application/x-www-form-urlencoded'}
         )
-        # Save the token in session
         st.session_state['oauth_token'] = token
 
-        # Fetch user info
         user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
         st.session_state['user_info'] = user_info
 
-        # Fetch user email
         email_info = linkedin.get(
             'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))'
         ).json()
@@ -52,12 +45,10 @@ def fetch_token_and_user_info(code):
 
     except HTTPError as e:
         st.error(f'An HTTP error occurred: {e.response.status_code}')
-        # Print the full error response for debugging
-        st.write(e.response.text)
+        st.write(e.response.text)  # Debug print
     except Exception as e:
         st.error(f'An error occurred: {e}')
-        # Print the exception message for debugging
-        st.write(str(e))
+        st.write(str(e))  # Debug print
 
 # Main App
 def main():
@@ -66,15 +57,16 @@ def main():
     query_params = get_query_params()
     code = query_params.get("code")
 
-    if code:
-        code = code[0]  # Get the first item from the list
-
     if "oauth_token" not in st.session_state:
         if not code:
             start_oauth()
         else:
-            fetch_token_and_user_info(code)
-            st.success("Authentication successful!")
+            # Extra check to make sure the code looks valid
+            if len(code) > 10:  # Usually, an auth code is a long string, not just one character
+                fetch_token_and_user_info(code)
+                st.success("Authentication successful!")
+            else:
+                st.error("The authorization code seems too short. Please try authenticating again.")
 
     if "user_info" in st.session_state:
         st.write("Your LinkedIn profile information:")
