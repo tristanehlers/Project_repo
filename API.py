c506677bf1061dae3262331c50cd966621142241
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-04 22:12:05 +0100

    s

--- API.py
+++ API.py
@@ -1,31 +1,21 @@
-import streamlit as st
+import requests
 from requests_oauthlib import OAuth2Session
-from oauthlib.oauth2 import MobileApplicationClient
+from oauthlib.oauth2 import BackendApplicationClient
 
-# OAuth 2.0 Client ID and Secret
-CLIENT_ID = 'YOUR_CLIENT_ID'
-CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
-REDIRECT_URI = 'YOUR_REDIRECT_URI'
+# Your LinkedIn credentials
+client_id = '785jejrypgi7ks'
+client_secret = '4ZwcgJ0s0ENgcVuA'
 
-# OAuth endpoints given in the XING API documentation
-authorization_base_url = 'https://api.xing.com/auth'
-token_url = 'https://api.xing.com/token'
+# Initialize OAuth2 client
+client = BackendApplicationClient(client_id=client_id)
+oauth = OAuth2Session(client=client)
+token = oauth.fetch_token(token_url='https://www.linkedin.com/oauth/v2/accessToken',
+                          client_id=client_id,
+                          client_secret=client_secret)
 
-# Using OAuth2Session for OAuth 2.0
-client = MobileApplicationClient(CLIENT_ID)
-oauth = OAuth2Session(client=client, redirect_uri=REDIRECT_URI)
+# Make a request to LinkedIn API
+response = oauth.get('API_ENDPOINT_HERE')
 
-# Redirect user to XING for authorization
-authorization_url, state = oauth.authorization_url(authorization_base_url)
-st.write(f'Please go here and authorize: {authorization_url}')
-
-# Get the authorization verifier code from the callback URL
-redirect_response = input('Paste the full redirect URL here: ')
-oauth.fetch_token(token_url, client_secret=CLIENT_SECRET,
-                  authorization_response=redirect_response)
-
-# Fetch a protected resource, i.e., user profile
-r = oauth.get('https://api.xing.com/v1/users/me')
-
-# Display in Streamlit
-st.json(r.json())
+# Process the response
+data = response.json()
+print(data)
