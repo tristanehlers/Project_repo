f7a07f841987d8f864a6d4191d7fbd227969a5ab
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-04 21:42:28 +0100

    s

--- API.py
+++ API.py
@@ -1,55 +1,31 @@
 import streamlit as st
 from requests_oauthlib import OAuth2Session
-from requests.exceptions import HTTPError
+from oauthlib.oauth2 import MobileApplicationClient
 
-# Constants
-CLIENT_ID = '785jejrypgi7s'
-CLIENT_SECRET = '4ZwcgJ0s0ENgcVuA'
-REDIRECT_URI = 'https://kup7u2ixdrj2gdn6wmq3er.streamlit.app/'
-AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
-TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-SCOPE = 'r_liteprofile r_emailaddress'
+# OAuth 2.0 Client ID and Secret
+CLIENT_ID = 'YOUR_CLIENT_ID'
+CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+REDIRECT_URI = 'YOUR_REDIRECT_URI'
 
-# Function to initiate OAuth process and handle token retrieval
-def handle_oauth():
-    linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE.split())
-    if "code" not in st.experimental_get_query_params():
-        authorization_url, state = linkedin.authorization_url(AUTHORIZATION_BASE_URL)
-        st.session_state['oauth_state'] = state
-        st.markdown(f"[Log in with LinkedIn]({authorization_url})", unsafe_allow_html=True)
-    else:
-        try:
-            token = linkedin.fetch_token(
-                TOKEN_URL,
-                client_secret=CLIENT_SECRET,
-                authorization_response=st.experimental_get_query_params()["code"][0]
-            )
-            st.session_state['oauth_token'] = token
-            st.write("Received token:", token)  # Display token
+# OAuth endpoints given in the XING API documentation
+authorization_base_url = 'https://api.xing.com/auth'
+token_url = 'https://api.xing.com/token'
 
-            # Fetch user profile information
-            profile_info = linkedin.get('https://api.linkedin.com/v2/me').json()
-            st.write("Your LinkedIn profile information:", profile_info)
+# Using OAuth2Session for OAuth 2.0
+client = MobileApplicationClient(CLIENT_ID)
+oauth = OAuth2Session(client=client, redirect_uri=REDIRECT_URI)
 
-            # Fetch user email address
-            email_info = linkedin.get('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))').json()
-            st.write("Your LinkedIn email address:", email_info.get('elements', [])[0].get('handle~', {}).get('emailAddress', ''))
+# Redirect user to XING for authorization
+authorization_url, state = oauth.authorization_url(authorization_base_url)
+st.write(f'Please go here and authorize: {authorization_url}')
 
-        except HTTPError as e:
-            st.error(f'HTTP error occurred: {e.response.status_code}')
-            st.error(e.response.text)
-        except Exception as e:
-            st.error(f'An error occurred: {e}')
+# Get the authorization verifier code from the callback URL
+redirect_response = input('Paste the full redirect URL here: ')
+oauth.fetch_token(token_url, client_secret=CLIENT_SECRET,
+                  authorization_response=redirect_response)
 
-# Main App
-def main():
-    st.title("LinkedIn OpenID Connect Authentication")
-    if 'oauth_token' not in st.session_state:
-        handle_oauth()
-    else:
-        # If token exists in session state, show user information
-        st.write("You are already authenticated")
-        st.json(st.session_state['oauth_token'])
+# Fetch a protected resource, i.e., user profile
+r = oauth.get('https://api.xing.com/v1/users/me')
 
-if __name__ == "__main__":
-    main()
+# Display in Streamlit
+st.json(r.json())
