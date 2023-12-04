37aadf7cfd9b2c363f406e21f4505a8939b13ea8
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 00:46:25 +0100

    j

--- API.py
+++ API.py
@@ -1,8 +1,14 @@
 import streamlit as st
-from careerjet_api_client import CareerjetAPIClient
+
+try:
+    from careerjet_api_client import CareerjetAPIClient
+except ImportError as e:
+    st.error(f"An error occurred while importing CareerjetAPIClient: {e}")
+    # Add any necessary logic to handle the import error here
+    raise
 
 # Initialize the Careerjet API client with your locale
-cj = CareerjetAPIClient("en_GB") # or any other locale code
+cj = CareerjetAPIClient("en_GB")  # or any other locale code
 
 # Streamlit app layout
 st.title("Job Search with Careerjet")
@@ -13,16 +19,21 @@ keywords = st.text_input("Keywords", "python")
 
 # Button to perform the search
 if st.button("Search Jobs"):
-    # Using the Careerjet API client to search for jobs
-    result_json = cj.search({
-        'location': location,
-        'keywords': keywords,
-        'affid': '213e213hd12344552', # Replace with your affiliate ID
-        'user_ip': '192.168.1.29', # Replace with the user's IP
-        'url': 'http://www.example.com/jobsearch', # Replace with the URL where the results will be displayed
-        'user_agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
-        'pagesize': 10,
-    })
+    try:
+        # Using the Careerjet API client to search for jobs
+        result_json = cj.search({
+            'location': location,
+            'keywords': keywords,
+            'affid': '213e213hd12344552',  # Replace with your affiliate ID
+            'user_ip': '192.168.1.29',  # Replace with the user's IP
+            'url': 'http://www.example.com/jobsearch',  # Replace with the URL where the results will be displayed
+            'user_agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
+            'pagesize': 10,
+        })
+    except Exception as e:
+        st.error(f"An error occurred while searching for jobs: {e}")
+        # Add any necessary logic to handle the search error here
+        raise
 
     # Displaying the search results
     if result_json and 'jobs' in result_json:
