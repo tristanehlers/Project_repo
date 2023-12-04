384e2e023fd2761943b848055e44fdd853738017
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 00:01:18 +0100

    careerjet

--- API.py
+++ API.py
@@ -1,70 +1,34 @@
 import streamlit as st
-from requests_oauthlib import OAuth2Session
-from requests.exceptions import HTTPError
+from careerjet_api_client import CareerjetAPIClient
 
-# Constants
-CLIENT_ID = '78nz3x6cxyj37h'
-CLIENT_SECRET = 'UofXEjpk0jez4CfI'
-REDIRECT_URI = 'https://kup7u2ixdrj2gdn6wmq3er.streamlit.app/'  # Your Streamlit app's address
-AUTHORIZATION_BASE_URL = 'https://www.linkedin.com/oauth/v2/authorization'
-TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-SCOPE = ['openid', 'profile', 'email']  # Scopes for OpenID Connect
+def search_jobs(location, keyword):
+    cj  = CareerjetAPIClient("en_GB")  # Use the appropriate locale here
 
-# Function to parse query string
-def get_query_params():
-    return st.experimental_get_query_params()
+    result = cj.search({
+        'location'    : location,
+        'keywords'    : keyword,
+        'affid'       : 'your_affiliate_id',  # Replace with your affiliate ID
+        'user_ip'     : '11.22.33.44',        # Replace with the user's IP
+        'url'         : 'http://www.example.com/jobsearch', # Replace with your URL
+        'pagesize'    : 10,
+    })
 
-# Start the OAuth process
-def start_oauth():
-    linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
-    authorization_url, state = linkedin.authorization_url(AUTHORIZATION_BASE_URL)
-    # Save the state in session for later use
-    st.session_state['oauth_state'] = state
-    # Redirect to LinkedIn for authorization
-    st.markdown(f"[Log in with LinkedIn]({authorization_url})", unsafe_allow_html=True)
+    return result['jobs']
 
-# Fetch token and user info
-def fetch_token_and_user_info(code):
-    try:
-        linkedin = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
-        # Add the client_secret parameter to the fetch_token() method call
-        token = linkedin.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, code=code)
-        # Save the token in session
-        st.session_state['oauth_token'] = token
+# Streamlit UI
+st.title("Job Search with Careerjet")
 
-        # Fetch user info
-        user_info = linkedin.get('https://api.linkedin.com/v2/me').json()
-        st.session_state['user_info'] = user_info
+location = st.text_input("Location")
+keyword = st.text_input("Keyword")
 
-        # Fetch user email
-        email_info = linkedin.get('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))').json()
-        st.session_state['email_info'] = email_info.get('elements', [])[0].get('handle~', {}).get('emailAddress', '')
-
-    except HTTPError as e:
-        st.error(f'An HTTP error occurred: {e.response.status_code}')
-    except Exception as e:
-        st.error(f'An error occurred: {e}')
-
-    
-# Main App
-def main():
-    st.title("LinkedIn OpenID Connect Authentication")
-
-    query_params = get_query_params()
-    code = query_params.get("code")
-
-    if "oauth_token" not in st.session_state:
-        if not code:
-            start_oauth()
-        else:
-            
-            st.success("Authentication successful!")
-
-    if "user_info" in st.session_state:
-        st.write("Your LinkedIn profile information:")
-        st.json(st.session_state['user_info'])
-
-        st.write("Your LinkedIn email information:")
-        st.write(st.session_state['email_info'])
-
-main()
+if st.button("Search"):
+    if location and keyword:
+        jobs = search_jobs(location, keyword)
+        for job in jobs:
+            st.write(job['title'], "-", job['company'])
+            st.write(job['description'])
+            st.write("Location:", job['locations'])
+            st.write("URL:", job['url'])
+            st.write("--------")
+    else:
+        st.warning("Please enter both location and keyword")
