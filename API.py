436b40a907ba9bc7cfb6563ac3ea91de27ca547f
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-04 22:56:24 +0100

    j

--- API.py
+++ API.py
@@ -1,4 +1,3 @@
-import requests
 import streamlit as st
 from requests_oauthlib import OAuth2Session
 
@@ -14,8 +13,9 @@ redirect_uri = 'https://kup7u2ixdrj2gdn6wmq3er.streamlit.app/'
 # Initialize OAuth2 session with the correct scopes
 oauth = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri, scope=['openid profile email'])
 
-# Generate the authorization URL
+# Generate the authorization URL and save the state
 authorization_url, state = oauth.authorization_url(authorization_base_url)
+st.session_state['oauth_state'] = state
 
 # Streamlit app to display authorization URL
 st.write('Please go to this URL and authorize:', authorization_url)
@@ -24,17 +24,17 @@ st.write('Please go to this URL and authorize:', authorization_url)
 redirect_response = st.text_input('Paste the callback URL here: ')
 
 if redirect_response:
+    # Retrieve the state from the session
+    state = st.session_state['oauth_state']
     # Exchange authorization code for access token
-    token = oauth.fetch_token(token_url, client_secret=client_secret, authorization_response=redirect_response)
+    token = oauth.fetch_token(token_url, client_secret=client_secret, 
+                              authorization_response=redirect_response,
+                              state=state)
 
     # Use token to make LinkedIn API calls
     linkedin = OAuth2Session(client_id, token=token)
-    # Make sure to include the access token in the header
-    response = linkedin.get('https://api.linkedin.com/v2/userinfo', headers={'Authorization': f'Bearer {token["access_token"]}'})
-    
-    # Check if the response was successful
-    if response.status_code == 200:
-        # Display user profile data in Streamlit
-        st.json(response.json())
-    else:
-        st.error('Failed to retrieve data from LinkedIn API')
+    # Fetch the user's profile data
+    response = linkedin.get('https://api.linkedin.com/v2/me')
+
+    # Display user profile data in Streamlit
+    st.json(response.json())
