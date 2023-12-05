a444cabf35a7def0681cb3c169aa2d82e9efaff4
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 02:43:43 +0100

    j

--- API.py
+++ API.py
@@ -11,12 +11,6 @@ api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
 # Set the geo_id parameter which the user cannot change
 geo_id = '101282230'
 
-# Initialize session state variables
-if 'jobs' not in st.session_state:
-    st.session_state['jobs'] = []
-    st.session_state['next_page_url'] = None
-    st.session_state['search_initiated'] = False
-
 # Function to display jobs
 def display_jobs(jobs):
     for job in jobs:
@@ -26,18 +20,28 @@ def display_jobs(jobs):
         st.write(f"[Job Details]({job['job_url']})")
         st.write("---------")
 
-# Create search fields for user input
-job_type = st.selectbox('Job Type', ['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer'])
-experience_level = st.selectbox('Experience Level', ['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director'])
-when = st.selectbox('When', ['anytime', 'yesterday', 'past-week', 'past-month'])
-flexibility = st.selectbox('Flexibility', ['anything', 'remote', 'on-site', 'hybrid'])
-keyword = st.text_input('Keyword', '')
+# Initialize session state variables
+if 'jobs' not in st.session_state:
+    st.session_state['jobs'] = []
+    st.session_state['next_page_url'] = None
+    st.session_state['search_initiated'] = False
+
+# Input fields container
+input_container = st.container()
+with input_container:
+    # Create search fields for user input
+    job_type = st.selectbox('Job Type', ['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer'])
+    experience_level = st.selectbox('Experience Level', ['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director'])
+    when = st.selectbox('When', ['anytime', 'yesterday', 'past-week', 'past-month'])
+    flexibility = st.selectbox('Flexibility', ['anything', 'remote', 'on-site', 'hybrid'])
+    keyword = st.text_input('Keyword', '')
+    search_button = st.button('Search Jobs')
 
 # Container to display jobs below the input fields
 jobs_container = st.container()
 
 # Button to perform the API call
-if st.button('Search Jobs'):
+if search_button:
     st.session_state['search_initiated'] = True
     st.session_state['jobs'] = []  # Clear previous jobs
     params = {
@@ -73,4 +77,5 @@ def load_more_jobs():
 
 # Show the 'Load More' button only if a search has been initiated and there's a next page URL
 if st.session_state['search_initiated'] and st.session_state['next_page_url']:
-    st.button('Load More', on_click=load_more_jobs)
+    with jobs_container:
+        st.button('Load More', on_click=load_more_jobs)
