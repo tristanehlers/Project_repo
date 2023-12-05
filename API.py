37e3a90e0bdc507752bf8119499086948ef220a8
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 17:12:11 +0100

    s

--- API.py
+++ API.py
@@ -6,6 +6,9 @@ api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'
 headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
 
+# URL of the LinkedIn logo
+logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
+
 # Country to geo_id mapping
 country_geo_id_mapping = {
     'Germany': '101282230',
@@ -16,9 +19,26 @@ country_geo_id_mapping = {
     'Italy': '103350119'
 }
 
-# Function to capitalize labels
-def capitalize_labels(options):
-    return [option.replace('_', ' ').title() for option in options]
+# Initialize session state variables
+if 'jobs' not in st.session_state:
+    st.session_state['jobs'] = []
+    st.session_state['next_page_url'] = None
+    st.session_state['search_initiated'] = False
+
+# Layout for the title and logo
+col1, col2 = st.columns([8, 2], gap="small")
+with col1:
+    st.markdown("# Job Search", unsafe_allow_html=True)
+with col2:
+    st.image(logo_url, width=60)  # Adjust the size to 40% of the original
+
+# Create search fields for user input
+country = st.selectbox('Country', list(country_geo_id_mapping.keys()))
+job_type = st.selectbox('Job Type', list(country_geo_id_mapping.keys()))
+experience_level = st.selectbox('Experience Level', list(country_geo_id_mapping.keys()))
+when = st.selectbox('When', list(country_geo_id_mapping.keys()))
+flexibility = st.selectbox('Flexibility', list(country_geo_id_mapping.keys()))
+keyword = st.text_input('Keyword', '')
 
 # Function to display jobs
 def display_jobs(jobs, container):
@@ -29,43 +49,19 @@ def display_jobs(jobs, container):
         container.write(f"[Job Details]({job['job_url']})")
         container.write("---------")
 
-# Initialize session state variables
-if 'jobs' not in st.session_state:
-    st.session_state['jobs'] = []
-    st.session_state['next_page_url'] = None
-    st.session_state['search_initiated'] = False
-
-# Title and logo layout
-col1, col2 = st.columns([8, 2])
-with col1:
-    st.write("")
-with col2:
-    st.image('https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png', width=60)
-
-# Adjusting the title position
-st.markdown("<h1 style='text-align: center; margin-bottom: -20px;'>Job Search</h1>", unsafe_allow_html=True)
-
-# Create search fields for user input
-country = st.selectbox('Country', list(country_geo_id_mapping.keys()))
-job_type = st.selectbox('Job Type', capitalize_labels(['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer']))
-experience_level = st.selectbox('Experience Level', capitalize_labels(['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director']))
-when = st.selectbox('When', capitalize_labels(['anytime', 'yesterday', 'past-week', 'past-month']))
-flexibility = st.selectbox('Flexibility', capitalize_labels(['anything', 'remote', 'on-site', 'hybrid']))
-keyword = st.text_input('Keyword', '')
-
-# Container for displaying jobs, placed in the right order
+# Container to display jobs below the input fields
 jobs_container = st.container()
 
 # Button to perform the API call
 if st.button('Search Jobs'):
     st.session_state['search_initiated'] = True
-    st.session_state['jobs'] = []  # Clear previous jobs
+    st.session_state['jobs'] = []
     selected_geo_id = country_geo_id_mapping[country]
     params = {
-        'job_type': job_type.lower().replace(' ', '_'),
-        'experience_level': experience_level.lower().replace(' ', '_'),
-        'when': when.lower().replace(' ', '_'),
-        'flexibility': flexibility.lower().replace(' ', '_'),
+        'job_type': job_type,
+        'experience_level': experience_level,
+        'when': when,
+        'flexibility': flexibility,
         'geo_id': selected_geo_id,
         'keyword': keyword
     }
