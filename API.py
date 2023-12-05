32d0570e7990c6631abb69e0753e25552b6c5753
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 02:33:05 +0100

    y

--- API.py
+++ API.py
@@ -20,10 +20,18 @@ def display_jobs(jobs, container):
         container.write(f"[Job Details]({job['job_url']})")
         container.write("---------")
 
-# Initialize session state variables
-if 'search_initiated' not in st.session_state:
-    st.session_state['search_initiated'] = False
-    st.session_state['next_page_url'] = None
+# Function to load more jobs
+def load_more_jobs():
+    with jobs_container:
+        next_page_url = st.session_state['next_page_url']
+        if next_page_url:
+            response = requests.get(next_page_url, headers=headers)
+            if response.status_code == 200:
+                jobs = response.json().get('job', [])
+                display_jobs(jobs, jobs_container)
+                st.session_state['next_page_url'] = response.json().get('next_page_api_url')
+            else:
+                st.error(f"Failed to load more jobs: {response.status_code}")
 
 # Create search fields for user input
 job_type = st.selectbox('Job Type', ['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer'])
@@ -32,26 +40,9 @@ when = st.selectbox('When', ['anytime', 'yesterday', 'past-week', 'past-month'])
 flexibility = st.selectbox('Flexibility', ['anything', 'remote', 'on-site', 'hybrid'])
 keyword = st.text_input('Keyword', '')
 
-# Function to load more jobs
-def load_more_jobs():
-    next_page_url = st.session_state.get('next_page_url')
-    if next_page_url:
-        response = requests.get(next_page_url, headers=headers)
-        if response.status_code == 200:
-            jobs = response.json().get('job', [])
-            display_jobs(jobs, jobs_container)
-            st.session_state['next_page_url'] = response.json().get('next_page_api_url')
-        else:
-            st.error(f"Failed to load more jobs: {response.status_code}")
-
-# Container to display jobs below the input fields
-jobs_container = st.container()
-
-# Button to perform the API call
+# Button to perform the API call and container to display jobs
 if st.button('Search Jobs'):
     st.session_state['search_initiated'] = True  # Flag that search was initiated
-    st.session_state['next_page_url'] = None  # Reset the next page URL
-
     # Make the API call with the user input, including the geo_id parameter
     params = {
         'job_type': job_type,
@@ -61,20 +52,20 @@ if st.button('Search Jobs'):
         'geo_id': geo_id,
         'keyword': keyword
     }
-    
     response = requests.get(api_endpoint, params=params, headers=headers)
-    
-    # Check if the request was successful
     if response.status_code == 200:
         jobs = response.json().get('job', [])
-        display_jobs(jobs, jobs_container)
-        
-        # Store the next page URL in the session state if there are more pages
-        st.session_state['next_page_url'] = response.json().get('next_page_api_url')
-
+        if jobs:
+            display_jobs(jobs, jobs_container)
+            st.session_state['next_page_url'] = response.json().get('next_page_api_url')
+        else:
+            st.write("No jobs found.")
     else:
         st.error(f"Failed to retrieve jobs: {response.status_code}")
 
+# Container to display jobs below the input fields
+jobs_container = st.container()
+
 # Only show the 'Load More' button if the search has been initiated and there's a next page URL available
-if st.session_state['search_initiated'] and st.session_state['next_page_url']:
+if st.session_state.get('search_initiated', False) and st.session_state.get('next_page_url'):
     st.button('Load More', on_click=load_more_jobs)
