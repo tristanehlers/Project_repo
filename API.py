87b5bb4a6349667d9f2364300d1f1cee2ed0f74d
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 17:25:31 +0100

    d

--- API.py
+++ API.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import requests
 
+# URL of the LinkedIn logo
+logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
+
 # Define the API key and headers
 api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'
 headers = {'Authorization': 'Bearer ' + api_key}
@@ -16,9 +19,26 @@ country_geo_id_mapping = {
     'Italy': '103350119'
 }
 
-# Function to capitalize labels
-def capitalize_labels(options):
-    return [option.replace('_', ' ').title() for option in options]
+# Initialize session state variables
+if 'jobs' not in st.session_state:
+    st.session_state['jobs'] = []
+    st.session_state['next_page_url'] = None
+    st.session_state['search_initiated'] = False
+
+# Title and logo layout
+header_col, logo_col = st.columns([0.85, 0.15])
+with header_col:
+    st.header("Job Search")
+with logo_col:
+    st.image(logo_url, width=60)  # Adjust width as needed
+
+# Create search fields for user input
+country = st.selectbox('Country', list(country_geo_id_mapping.keys()))
+job_type = st.selectbox('Job Type', ['Anything', 'Full Time', 'Part Time', 'Internship', 'Contract', 'Temporary', 'Volunteer'])
+experience_level = st.selectbox('Experience Level', ['Anything', 'Internship', 'Entry Level', 'Associate', 'Mid-Senior Level', 'Director'])
+when = st.selectbox('When', ['Anytime', 'Yesterday', 'Past-Week', 'Past-Month'])
+flexibility = st.selectbox('Flexibility', ['Anything', 'Remote', 'On-Site', 'Hybrid'])
+keyword = st.text_input('Keyword', '')
 
 # Function to display jobs
 def display_jobs(jobs, container):
@@ -29,43 +49,19 @@ def display_jobs(jobs, container):
         container.write(f"[Job Details]({job['job_url']})")
         container.write("---------")
 
-# Initialize session state variables
-if 'jobs' not in st.session_state:
-    st.session_state['jobs'] = []
-    st.session_state['next_page_url'] = None
-    st.session_state['search_initiated'] = False
-
-# Title and logo layout
-col1, col2 = st.columns([8, 2])
-with col1:
-    st.write("")
-with col2:
-    st.image('https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png', width=60)
-
-# Adjusting the title position
-st.markdown("<h1 style='text-align: center; margin-bottom: -20px;'>Job Search</h1>", unsafe_allow_html=True)
-
-# Create search fields for user input
-country = st.selectbox('Country', list(country_geo_id_mapping.keys()))
-job_type = st.selectbox('Job Type', capitalize_labels(['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer']))
-experience_level = st.selectbox('Experience Level', capitalize_labels(['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director']))
-when = st.selectbox('When', capitalize_labels(['anytime', 'yesterday', 'past-week', 'past-month']))
-flexibility = st.selectbox('Flexibility', capitalize_labels(['anything', 'remote', 'on-site', 'hybrid']))
-keyword = st.text_input('Keyword', '')
-
-# Container for displaying jobs, placed in the right order
+# Container to display jobs below the input fields
 jobs_container = st.container()
 
 # Button to perform the API call
 if st.button('Search Jobs'):
     st.session_state['search_initiated'] = True
     st.session_state['jobs'] = []  # Clear previous jobs
-    selected_geo_id = country_geo_id_mapping[country]
+    selected_geo_id = country_geo_id_mapping[country]  # Use the selected country's geo_id
     params = {
-        'job_type': job_type.lower().replace(' ', '_'),
-        'experience_level': experience_level.lower().replace(' ', '_'),
-        'when': when.lower().replace(' ', '_'),
-        'flexibility': flexibility.lower().replace(' ', '_'),
+        'job_type': job_type.replace(' ', '_').lower(),
+        'experience_level': experience_level.replace(' ', '_').lower(),
+        'when': when.replace(' ', '_').lower(),
+        'flexibility': flexibility.replace(' ', '_').lower(),
         'geo_id': selected_geo_id,
         'keyword': keyword
     }
