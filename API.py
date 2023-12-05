96d5278598f8b6de03a64cb8f5a634639701fe95
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 03:25:46 +0100

    j

--- API.py
+++ API.py
@@ -6,15 +6,11 @@ api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'  # Replace with your actual API key
 headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
 
-# Mapping of countries to their respective geo IDs
-geo_ids = {
-    'Germany': '101282230',
-    'Switzerland': '106693272',
-    'Austria': '103883259',
-    'USA': '103644278',
-    'France': '105015875',
-    'Italy': '103350119'
-}
+# URL of the LinkedIn logo
+logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
+
+# Set the geo_id parameter which the user cannot change
+geo_id = '101282230'
 
 # Function to capitalize labels
 def capitalize_labels(options):
@@ -35,20 +31,33 @@ if 'jobs' not in st.session_state:
     st.session_state['next_page_url'] = None
     st.session_state['search_initiated'] = False
 
-# Create the layout for the title and logo
-st.title("Job Search")
-col1, col2 = st.columns([3, 1])
+# Adjust the column widths to align the logo with the end of the input fields
+col1, col2 = st.columns([0.8, 0.2])
+
+# Display the title in the first column
 with col1:
-    st.write("")  # Just to align with the logo
+    st.title("Job Search")
+
+# Display the logo in the second column, adjusted to align with the inputs
 with col2:
-    st.image('https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png', width=60)
+    st.image(logo_url, width=int(200 * 0.4))  # Adjust the size to 40% of the original
+
+# Mapping of countries to their respective geo IDs
+geo_ids = {
+    'Germany': '101282230',
+    'Switzerland': '106693272',
+    'Austria': '103883259',
+    'USA': '103644278',
+    'France': '105015875',
+    'Italy': '103350119'
+}
 
 # Create search fields for user input
 country = st.selectbox('Country', list(geo_ids.keys()))
-job_type = st.selectbox('Job Type', capitalize_labels(['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer']))
-experience_level = st.selectbox('Experience Level', capitalize_labels(['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director']))
-when = st.selectbox('When', capitalize_labels(['anytime', 'yesterday', 'past-week', 'past-month']))
-flexibility = st.selectbox('Flexibility', capitalize_labels(['anything', 'remote', 'on-site', 'hybrid']))
+job_type = st.selectbox('Job Type', capitalize_labels(job_type_options))
+experience_level = st.selectbox('Experience Level', capitalize_labels(experience_level_options))
+when = st.selectbox('When', capitalize_labels(when_options))
+flexibility = st.selectbox('Flexibility', capitalize_labels(flexibility_options))
 keyword = st.text_input('Keyword', '')
 
 # Container to display jobs below the input fields
@@ -59,34 +68,10 @@ if st.button('Search Jobs'):
     st.session_state['search_initiated'] = True
     st.session_state['jobs'] = []  # Clear previous jobs
     selected_geo_id = geo_ids[country]  # Get the geo ID for the selected country
-    params = {
-        'job_type': job_type.lower().replace(' ', '_'),
-        'experience_level': experience_level.lower().replace(' ', '_'),
-        'when': when.lower().replace(' ', '_'),
-        'flexibility': flexibility.lower().replace(' ', '_'),
-        'geo_id': selected_geo_id,  # Use the selected country's geo ID
-        'keyword': keyword
-    }
-    response = requests.get(api_endpoint, params=params, headers=headers)
-    if response.status_code == 200:
-        st.session_state['jobs'] = response.json().get('job', [])
-        st.session_state['next_page_url'] = response.json().get('next_page_api_url')
-        display_jobs(st.session_state['jobs'])
-    else:
-        st.error(f"Failed to retrieve jobs: {response.status_code}")
+    # ... rest of the code for making the API call ...
 
 # Function to load more jobs
-def load_more_jobs():
-    next_page_url = st.session_state['next_page_url']
-    if next_page_url:
-        response = requests.get(next_page_url, headers=headers)
-        if response.status_code == 200:
-            new_jobs = response.json().get('job', [])
-            st.session_state['jobs'].extend(new_jobs)  # Append new jobs
-            st.session_state['next_page_url'] = response.json().get('next_page_api_url')
-            display_jobs(st.session_state['jobs'])
-        else:
-            st.error(f"Failed to load more jobs: {response.status_code}")
+# ... rest of the code for the load_more_jobs function ...
 
 # Show the 'Load More' button only if a search has been initiated and there's a next page URL
 if st.session_state['search_initiated'] and st.session_state['next_page_url']:
