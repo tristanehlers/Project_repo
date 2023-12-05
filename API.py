d3dc9a3e00c27925b1740ac4c434e13b56ca8ecf
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-05 16:43:58 +0100

    j

--- API.py
+++ API.py
@@ -6,11 +6,15 @@ api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'  # Replace with your actual API key
 headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
 
-# URL of the LinkedIn logo
-logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
-
-# Set the geo_id parameter which the user cannot change
-geo_id = '101282230'
+# Country to geo_id mapping
+country_geo_id_mapping = {
+    'Germany': '101282230',
+    'Switzerland': '106693272',
+    'Austria': '103883259',
+    'USA': '103644278',
+    'France': '105015875',
+    'Italy': '103350119'
+}
 
 # Function to capitalize labels
 def capitalize_labels(options):
@@ -31,18 +35,15 @@ if 'jobs' not in st.session_state:
     st.session_state['next_page_url'] = None
     st.session_state['search_initiated'] = False
 
-# Adjust the column widths to align the logo with the end of the input fields
-col1, col2 = st.columns([0.9, 0.1])
-
-# Display the title in the first column
+# Title and logo
+col1, col2 = st.columns([8, 2])
 with col1:
-    st.markdown("<h1 style='text-align: left;'>Job Search</h1>", unsafe_allow_html=True)
-
-# Display the logo in the second column
+    st.title("Job Search")
 with col2:
-    st.image(logo_url, width=60)  # Reduce the size of the picture by 60%
+    st.image(logo_url, width=60)  # The logo is displayed at a reduced size
 
 # Create search fields for user input
+country = st.selectbox('Country', list(country_geo_id_mapping.keys()))
 job_type = st.selectbox('Job Type', capitalize_labels(['anything', 'full_time', 'part_time', 'internship', 'contract', 'temporary', 'volunteer']))
 experience_level = st.selectbox('Experience Level', capitalize_labels(['anything', 'internship', 'entry_level', 'associate', 'mid_senior_level', 'director']))
 when = st.selectbox('When', capitalize_labels(['anytime', 'yesterday', 'past-week', 'past-month']))
@@ -55,13 +56,14 @@ jobs_container = st.container()
 # Button to perform the API call
 if st.button('Search Jobs'):
     st.session_state['search_initiated'] = True
-    st.session_state['jobs'] = []  # Clear previous jobs
+    st.session_state['jobs'] = []
+    selected_geo_id = country_geo_id_mapping[country]  # Use the selected country's geo_id
     params = {
         'job_type': job_type.lower().replace(' ', '_'),
         'experience_level': experience_level.lower().replace(' ', '_'),
         'when': when.lower().replace(' ', '_'),
         'flexibility': flexibility.lower().replace(' ', '_'),
-        'geo_id': geo_id,
+        'geo_id': selected_geo_id,
         'keyword': keyword
     }
     response = requests.get(api_endpoint, params=params, headers=headers)
@@ -79,7 +81,7 @@ def load_more_jobs():
         response = requests.get(next_page_url, headers=headers)
         if response.status_code == 200:
             new_jobs = response.json().get('job', [])
-            st.session_state['jobs'].extend(new_jobs)  # Append new jobs
+            st.session_state['jobs'].extend(new_jobs)
             st.session_state['next_page_url'] = response.json().get('next_page_api_url')
             display_jobs(st.session_state['jobs'])
         else:
