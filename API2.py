b2408b2d05ba11d3aa33e9018c60a63edf8c7fe2
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-06 03:01:24 +0100

    c

--- API2.py
+++ API2.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # Define your API key and headers
-api_key = 'OQfhKnmj2k9bUHmlHH9Qbg'  # Be sure to replace with your actual API key
+api_key = '_EIqMpWEbOnJLoQvNFz1CQ'  # Be sure to replace with your actual API key
 headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
 
@@ -16,16 +16,7 @@ def extract_info(jsondata):
     }
     return extracted_info
 
-# Streamlit app layout
-st.title('LinkedIn Profile Filler')
-
-# Input field for LinkedIn profile URL
-linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', 'https://www.linkedin.com/in/...')
-
-# Display the clickable image
-if st.button('Retrieve Information'):
-    retrieve_info()
-
+# Function to retrieve information
 def retrieve_info():
     params = {'linkedin_profile_url': linkedin_profile_url}
     
@@ -50,4 +41,12 @@ def retrieve_info():
     else:
         st.error(f"Failed to retrieve profile information: HTTP {response.status_code}")
 
-# Load the image from the
+# Streamlit app layout
+st.title('LinkedIn Profile Filler')
+
+# Input field for LinkedIn profile URL
+linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', 'https://www.linkedin.com/in/...')
+
+# Button to trigger the information retrieval
+if st.button('Retrieve Information'):
+    retrieve_info()
