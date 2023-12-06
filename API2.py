ce428e443970cf1ed0a9e3a2a5eb9e4ba2ae19e4
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-06 03:04:43 +0100

    s

--- API2.py
+++ API2.py
@@ -19,33 +19,32 @@ def extract_info(jsondata):
 # Function to retrieve information
 def retrieve_info():
     params = {'linkedin_profile_url': linkedin_profile_url}
-    
-    # Make a request to the API
     response = requests.get(api_endpoint, params=params, headers=headers)
-    
     if response.status_code == 200:
         data = response.json()
         info = extract_info(data)
-        
-        # Display the extracted information
-        st.subheader('Profile Information:')
-        st.write('Full Name:', info['full_name'])
-        st.write('City:', info['city'])
-        
-        st.subheader('Experiences:')
-        for exp in info['experiences']:
-            st.write('Title:', exp.get('title', 'Not available'))
-            st.write('Company:', exp.get('company', 'Not available'))
-            st.write('Description:', exp.get('description', 'Not available'))
-            st.write('---')  # Separator line
+        display_info(info)
     else:
         st.error(f"Failed to retrieve profile information: HTTP {response.status_code}")
+
+# Function to display profile information
+def display_info(info):
+    st.subheader('Profile Information:')
+    st.write('Full Name:', info['full_name'])
+    st.write('City:', info['city'])
+    
+    st.subheader('Experiences:')
+    for exp in info['experiences']:
+        st.write('Title:', exp.get('title', 'Not available'))
+        st.write('Company:', exp.get('company', 'Not available'))
+        st.write('Description:', exp.get('description', 'Not available'))
+        st.write('---')  # Separator line
 
 # Streamlit app layout
 st.title('LinkedIn Profile Filler')
 
-# Input field for LinkedIn profile URL
-linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', 'https://www.linkedin.com/in/...')
+# Input field for LinkedIn profile URL without a prefill
+linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL')
 
 # Button to trigger the information retrieval
 if st.button('Retrieve Information'):
