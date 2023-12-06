5e476d9f423706678c51feb0e7a9b8ee80ef0dc2
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-06 02:56:40 +0100

    x

--- API2.py
+++ API2.py
@@ -17,32 +17,45 @@ def extract_info(jsondata):
     return extracted_info
 
 # Streamlit app layout
-st.title('LinkedIn Profile Filler')
+st.set_page_config(layout="wide")  # Set the page to wide layout
+
+# Use columns to position the header and logo
+col1, col2 = st.columns([5, 1])  # Adjust the ratio as needed for your layout
+
+with col1:
+    st.title('LinkedIn Profile Filler')
+
+with col2:
+    linkedin_logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
+    st.image(linkedin_logo_url, width=50)
 
 # Input field for LinkedIn profile URL
 linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', 'https://www.linkedin.com/in/...')
 
-# Button to fetch profile information
+# Button to trigger the information retrieval
 if st.button('Retrieve Information'):
+    retrieve_info()
+
+# Function to fetch and display profile information
+def retrieve_info():
     params = {'linkedin_profile_url': linkedin_profile_url}
-    
-    # Make a request to the API
     response = requests.get(api_endpoint, params=params, headers=headers)
-    
     if response.status_code == 200:
         data = response.json()
         info = extract_info(data)
-        
-        # Display the extracted information
-        st.subheader('Profile Information:')
-        st.write('Full Name:', info['full_name'])
-        st.write('City:', info['city'])
-        
-        st.subheader('Experiences:')
-        for exp in info['experiences']:
-            st.write('Title:', exp.get('title', 'Not available'))
-            st.write('Company:', exp.get('company', 'Not available'))
-            st.write('Description:', exp.get('description', 'Not available'))
-            st.write('---')  # Separator line
+        display_info(info)
     else:
         st.error(f"Failed to retrieve profile information: HTTP {response.status_code}")
+
+# Function to display profile information
+def display_info(info):
+    st.subheader('Profile Information:')
+    st.write('Full Name:', info['full_name'])
+    st.write('City:', info['city'])
+    
+    st.subheader('Experiences:')
+    for exp in info['experiences']:
+        st.write('Title:', exp.get('title', 'Not available'))
+        st.write('Company:', exp.get('company', 'Not available'))
+        st.write('Description:', exp.get('description', 'Not available'))
+        st.write('---')  # Separator line
